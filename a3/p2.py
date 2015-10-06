30507abe2b7fcda985859ec7657afd56808555c9
dougshidong <doug.shidong@gmail.com>
2015-10-05 23:11:32 -0400

    working 2d newton

--- a3/p2.py
+++ a3/p2.py
@@ -1,39 +1,43 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-def fx(x): 
-    3*x[0]**2-y[0]**2
+def fun(x,y): 
+    y[0] = 3 * x[0]**2 - x[1]**2
+    y[1] = 3 * x[0] * x[1]**2 - x[0]**3 - 1
+    return
 
-dfdx = lambda x: -np.sin(x) - 1 
+def Jac(x,J):
+    J[0][0] = 6 * x[0]
+    J[0][1] = -2 * x[1]
+    J[1][0] = 3 * x[1]**2 - 3 * x[0]**2
+    J[1][1] = 6 * x[0] * x[1]
+    return
+
 
 maxit = 10
-tol = 1e-10
-x = [-1]
-
+tol = 1e-5
+x = np.zeros(shape=(maxit,2))
+x[0] = [1.,1.]
+x0 = np.empty(2)
+x1 = np.empty(2)
+fx = np.empty(2)
+J  = np.empty((2,2))
 for i in range(maxit):
+    print('Iteration: ',i+1)
     x0 = x[i]
-    x1 = x0 - fx(x0) / dfdx(x0)
-    if(abs(x0-x1) < tol):
+    print('x0:\n', x0 )
+    fun(x0,fx)
+    print('fx:\n', fx)
+    Jac(x0,J)
+    print('J:\n', J )
+    J = np.linalg.inv(J)
+    print('Jinv:\n', J )
+    x1 = x0 - np.dot(J, fx)
+    print('x1:\n', x1 )
+    if(np.linalg.norm(x0-x1) < tol):
         break
-    x.append(x1)
+    x[i+1] = x1
 
-xexact = np.pi/4
-
-print x
-
-print xexact
-
-xdat = np.linspace(0,1,50)
-
-cosplt  = plt.plot(xdat, np.cos(xdat), label = 'm(x) = cos(x)')
-xplt    = plt.plot(xdat, xdat, label = 'n(x) = x')
-xcosplt = plt.plot(xdat, fx(xdat), label = 'g(x) = cos(x) - x')
-plt.legend()
-xnewtplot = plt.plot(x, fx(x), '-o')
-
-plt.xlim(0.5,1.5)
-plt.ylim(-1,1)
-
-plt.show()
+print(x)
