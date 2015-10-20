6cb4e27254334bdfffe6af174a562a247f6a7fb8
dougshidong <doug.shidong@gmail.com>
2015-10-20 16:28:13 -0400

    need to fixed divdiff

--- a3/divdiff.py
+++ a3/divdiff.py
@@ -1,11 +1,26 @@
-#!/usr/bin/env/ python
+#!/usr/bin/env python
 import numpy as np
 
-data1 = np.array([0,1,2], dtype = np.float64)
+#data = np.array([[0,1,2],[3,5,-1]], dtype = np.float64)
 
-data2 = np.array([3,5,-1], dtype = np.float64)
+data = np.array([[0,1,3,4,7],[1,3,49,129,813]], dtype = np.float64)
 
+def ddiff(x1, x2,
+          y1, y2):
+    return (y2 - y1) / (x2 - x1)
+
+n = data.shape[1]
 x = 0.05
 
-for i in range(3)
-    
+coeff = data[1,:]
+
+for k in range(n-1):
+    print k
+    print data[0,:]
+    print coeff
+    for j, i in enumerate(range(k+1,n-1+1)):
+        print i,i-1
+        coeff[i] = (coeff[i] - coeff[i-1]) / (data[0,i] - data[0,j])
+                           
+print 'Coefficients:'        
+print coeff
