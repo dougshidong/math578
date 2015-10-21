b71b803f1674ae573876a61b8e69e4c1e9893e83
dougshidong <doug.shidong@gmail.com>
2015-10-21 12:00:52 -0400

    done divdiff

--- a3/divdiff.py
+++ a3/divdiff.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 
-#data = np.array([[0,1,2],[3,5,-1]], dtype = np.float64)
-
-data = np.array([[0,1,3,4,7],[1,3,49,129,813]], dtype = np.float64)
-
-def ddiff(x1, x2,
-          y1, y2):
-    return (y2 - y1) / (x2 - x1)
+data = np.array([[0,1,2,3],[3,5,-1,2]], dtype = np.float64)
 
 n = data.shape[1]
 x = 0.05
@@ -15,12 +9,13 @@ x = 0.05
 coeff = data[1,:]
 
 for k in range(n-1):
-    print k
-    print data[0,:]
-    print coeff
-    for j, i in enumerate(range(k+1,n-1+1)):
-        print i,i-1
-        coeff[i] = (coeff[i] - coeff[i-1]) / (data[0,i] - data[0,j])
+    coeff[k+1:n] = (coeff[k+1:n] - coeff[k:n-1]) / (data[0,k+1:n] - data[0,0:n-k-1])
                            
 print 'Coefficients:'        
 print coeff
+
+for ni in range (2,n+1):
+    fx = 0
+    for k in range(ni):
+        fx += np.prod(x - data[0,0:k]) * coeff[k]
+    print 'Degree %d Interpolation of %f is %f' % (ni-1,x, fx)
