e26107b29a85cf6ba14baa1c9d8d62a2e738f004
dougshidong <doug.shidong@gmail.com>
2015-10-22 17:39:49 -0400

    commit all

--- a3/invPowM.py
+++ a3/invPowM.py
@@ -3,30 +3,37 @@ import numpy as np
 
 def rayleigh(A,x):
     return np.dot(x, np.dot(A,x))
-    #/ np.dot(x,x)
+
+def powerIt(A,x):
+    y = np.dot(A,x)
+    return y / np.linalg.norm(y,2)
+
 # Matrix A
 A = np.array([[-4, 14, 0],
                [-5, 13, 0],
                [-1, 0 , 0]])
 
 # Convergence Information
-tol = 1e-10
-n = 1000
+tol = 1e-14
+n = 50
 eValResi  = 1
 eVecResi  = 1
 atkinResi = 1
 
 # Inverse Power Method Options
-useInv = 1
+useInv = 0 # Toggle on or off
 if(useInv == 1):
     mu = 3.5
-    Amu = A - np.identity(3) * mu
-    Ainv = np.linalg.solve(Amu,np.identity(3))
+    B = A - np.identity(3) * mu
+    Binv = np.linalg.solve(B,np.identity(3))
 
 # Atkin Divided Difference to Speed Up Eigenvalue Convergence
-useAtkin = 0
+useAtkin = 0 # Toggle on or off
 if(useAtkin == 1):
     at = np.empty(n)
+    xn0= np.empty(3)
+    xn1= np.empty(3)
+    xn2= np.empty(3)
 
 # Data Initialization
 # Eigenvectors
@@ -36,36 +43,44 @@ x[0] = np.array([1,1,1])
 ra  = np.empty(n)
 ra[0] = rayleigh(A,x[0])
 
+if(useAtkin ==1):
+    for i in range(n-1):
+        xn0 = x[i]
 
-for i in range(n-1):
-    if(useInv == 1):
-        y = np.dot(Ainv,x[i])
+        xn1 = powerIt(A,xn0)
+        
+        del1x = xn1-xn0
+        if(max(abs(del1x)) < tol):
+            break
+        
+        xn2 = powerIt(A,xn1)
+        del2x = xn0-2*xn1+xn2
+        
+        x[i+1] = xn0 - del1x**2/del2x
+
     else:
-        y = np.dot(A, x[i])
-
-    x[i+1] = y / np.linalg.norm(y,2)
-
-    ra[i+1] = rayleigh(A,x[i+1])
-    if(useAtkin == 1 and i>1 ):
-        an2 = ra[i]
-        an1 = ra[i-1]
-        an0 = ra[i-2]
-        at[i-2] = (an2 * an0 - an1 * an1) / (an2 - 2 * an1 + an0)
-        if(i>2):
-            atkinResi = abs(at[i-2] - at[i-3])
-
-    eValResi = abs(ra[i] - ra[i+1])
-    eVecResi = np.linalg.norm(abs(x[i]-x[i+1]),2)
-    if(min(min(eValResi,atkinResi),eVecResi)<tol):
-        break
+        print 'Did not converge'
 else:
-    print 'Did not converge'
+    for i in range(n-1):
+        if(useInv == 1):
+            x[i+1] = powerIt(Binv,x[i])
+        else:
+            x[i+1] = powerIt(A,x[i])
+
+        ra[i+1] = rayleigh(A,x[i+1])
+
+        eValResi = abs(ra[i] - ra[i+1])
+        if(eValResi < tol):
+            break
+    else:
+        print 'Did not converge'
+
 
 print 'Took %d iterations' %i
 print 'Eigenvalue'
-if(atkinResi>eValResi):
+if(useAtkin!=1):
     print ra[i+1]
 else:
-    print at[i-3]
+    print rayleigh(A,x[i])
 print 'Eigenvector:'
-print x[i+1]
+print x[i-1]
